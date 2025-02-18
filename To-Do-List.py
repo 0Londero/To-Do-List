731cd7ef170bf974b6d3c54ac7188a95fe921005
Otávio Londero <otavio.b.londero@gmail.com>
2025-02-18 14:20:10 -0800

    Main Structure

---
+++ To-Do-List.py
@@ -0,0 +1,9 @@
+"""
+Create a simple to-do list application that allows users to add, remove, and view
+tasks. This project will help you practice working with lists and functions in
+Python.
+"""
+
+# Initializing a global variable for saving the Task
+task = []
+
