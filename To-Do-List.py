2e10990515c343330589bae186b439e28e375083
mpzamos0297 <mariap.zamos0297@gmail.com>
2025-02-18 21:06:33 -0800

    Update To-Do-List.py
    Suggestion sort

--- To-Do-List.py
+++ To-Do-List.py
@@ -7,3 +7,22 @@ Python.
 # Initializing a global variable for saving the Task
 task = []
 
+#example of variables added - temporary information
+task.append({"name":"example", "priority": 3,"deadline":'2025-2-15'})
+task.append({"name":"example2", "priority": 1,"deadline":'2025-2-17'})
+task.append({"name":"example3", "priority": 3,"deadline":'2025-2-27'})
+task.append({"name":"example4", "priority": 1,"deadline":'2025-2-20'})
+task.append({"name":"example5", "priority": 1,"deadline":'2025-2-22'})
+print(len(task))
+choice = 4
+
+if choice == 4:
+    n = 0
+    if len(task) < 10:
+        n=len(task)
+        suggestion = sorted(task, key=lambda elem:(elem['deadline'],elem['priority']))[:n]
+    if len(task)>=10:
+        suggestion = sorted(task, key=lambda elem: (elem['deadline'], elem['priority']))[:10]
+    
+    print("Hi! Here are some tasks you might want to work on: ")
+    print(suggestion)
