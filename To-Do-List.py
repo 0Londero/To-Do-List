63c0d7c1c62ee65ba55e8c8e9f375ec857276fbd
Ann Chang <minianns48@gmail.com>
2025-02-19 18:36:38 -0800

    Update To-Do-List.py

--- To-Do-List.py
+++ To-Do-List.py
@@ -5,5 +5,48 @@ Python.
 """
 
 # Initializing a global variable for saving the Task
+from logging import exception
+
 task = []
+
+#Define add new list
+import datetime
+def add_task():
+
+#Task
+    while True:
+        new_task = input("Enter the task : ")
+        if new_task == " ":
+            print("\033[31mTask can't be empty\033[0m")
+            continue
+        else:
+            break
+
+#Priority
+    while True:
+        priority = input("Enter the priority (1 = high, 2 = medium, 3 = low) : ")
+        if priority not in ["1", "2", "3"] :
+            print("\033[31mInvalid priority. Please enter 1, 2, or 3\033[0m")
+            continue
+        else:
+            break
+
+#Date
+    while True:
+        date = input("Enter the deadline (YYYY-MM-DD) : ")
+        try:
+            deadline = datetime.datetime.strptime(date, "%Y-%m-%d").date()
+            today = datetime.date.today()
+            if deadline < today :
+                print("\033[31mThe deadline can't be past date.\033[0m")
+                continue
+            else:
+                break
+
+        except ValueError:
+            print("\033[31mInvalid deadline. Please enter in YYYY-MM-DD format.\033[0m")
+            continue
+
+    task.append({"Task" : new_task, "Priority" : priority, "Deadline" : date})
+    print(f'Your task : \033[33m{new_task}\033[0m with priority : \033[33m{priority}\033[0m and deadline : \033[33m{date}\033[0m has been added to the list.')
 
