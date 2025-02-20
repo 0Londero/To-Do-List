4d432e156ac142e421693090780c7c374ac65c27
Otávio Londero <otavio.b.londero@gmail.com>
2025-02-20 14:28:37 -0800

    Added menu validation and menu display file.

---
+++ feature_menu.py
@@ -0,0 +1,8 @@
+def display_menu():
+    print("""
+        #########  To-Do List  #########
+        1. Add Task
+        2. Remove Task
+        3. View Tasks / Suggest Tasks
+        4. Exit
+        """)
